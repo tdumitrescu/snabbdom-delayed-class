8d6e623685cf0dee418504e4fb233ed505caa647
t ddddddd <webdev@cmme.org>
2016-12-18 23:56:21 -0800

    test delayed class

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var delayedClass = require('.');
 var document = require('html-element').document;
 var expect = require('expect.js');
 var h = require('snabbdom/h');
+var raf = require('raf');
 var snabbdom = require('snabbdom');
 
 var patch = snabbdom.init([
@@ -29,5 +30,27 @@ describe('delayed-class module', function() {
     patch(vnode, h('div', {class: {'foo': false}}));
     expect(el.classList.contains('foo')).not.to.be.ok();
     expect(el.outerHTML).to.contain('<div>');
+  });
+
+  it('implicitly removes classes', function() {
+    var vnode = patch(el, h('div', {class: {'foo': true}}));
+    el = vnode.elm;
+    expect(el.classList.contains('foo')).to.be.ok();
+
+    patch(vnode, h('div'));
+    expect(el.classList.contains('foo')).not.to.be.ok();
+    expect(el.outerHTML).to.contain('<div>');
+  });
+
+  it('applies delayed class updates in the next frame', function(done) {
+    el = patch(el, h('div', {class: {delayed: {'foo': true}}})).elm;
+    expect(el.classList.contains('foo')).not.to.be.ok();
+
+    raf(function() {
+      raf(function() {
+        expect(el.classList.contains('foo')).to.be.ok();
+        done();
+      });
+    });
   });
 });
