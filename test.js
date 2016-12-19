8e76605fed59ec7213fdac74edd87badd0b612de
t ddddddd <webdev@cmme.org>
2016-12-18 19:45:12 -0800

    test for class updates

--- test.js
+++ test.js
@@ -21,4 +21,14 @@ describe('delayed-class module', function() {
     expect(el.classList.contains('foo')).to.be.ok();
     expect(el.outerHTML).to.contain('<div class="foo">');
   });
+
+  it('applies class updates', function() {
+    var vnode = patch(el, h('div', {class: {'foo': true}}));
+    el = vnode.elm;
+    expect(el.classList.contains('foo')).to.be.ok();
+
+    patch(vnode, h('div', {class: {'foo': false}}));
+    expect(el.classList.contains('foo')).not.to.be.ok();
+    expect(el.outerHTML).to.contain('<div>');
+  });
 });
