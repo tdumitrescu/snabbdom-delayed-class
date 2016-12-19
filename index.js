4afe30489d4cc360db83cc4da25fd7878c864ca5
t ddddddd <webdev@cmme.org>
2016-12-18 19:39:06 -0800

    make all the things a var

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var raf = (typeof requestAnimationFrame !== 'undefined' && requestAnimationFrame) || setTimeout;
 
 function addOrRemoveClass(el, className, add) {
@@ -17,9 +19,9 @@ function changeClassNextFrame(el, className, add) {
 }
 
 function updateClass(oldVnode, newVnode) {
-  const el = newVnode.elm;
-  let oldClass = oldVnode.data.class;
-  let newClass = newVnode.data.class;
+  var el = newVnode.elm;
+  var oldClass = oldVnode.data.class;
+  var newClass = newVnode.data.class;
 
   if (!oldClass && !newClass) {
     return;
@@ -28,23 +30,23 @@ function updateClass(oldVnode, newVnode) {
   newClass = newClass || {};
 
   // remove classes which are no longer in vnode class obj
-  for (const name in oldClass) {
+  for (var name in oldClass) {
     if (!(name in newClass)) {
       el.classList.remove(name);
     }
   }
 
   // handle new classes, taking into account special delayed classes
-  const oldHasDelay = 'delayed' in oldClass;
-  for (const name in newClass) {
-    const val = newClass[name];
+  var oldHasDelay = 'delayed' in oldClass;
+  for (var name in newClass) {
+    var val = newClass[name];
 
     if (typeof val === 'object') {
 
       // special 'delayed'/'remove' class objects
       if (name === 'delayed') {
-        for (const delayedName in val) {
-          const delayedVal = val[delayedName];
+        for (var delayedName in val) {
+          var delayedVal = val[delayedName];
           if (!oldHasDelay || delayedVal !== oldClass.delayed[delayedName]) {
             changeClassNextFrame(el, delayedName, delayedVal);
           }
@@ -61,30 +63,30 @@ function updateClass(oldVnode, newVnode) {
 }
 
 function applyDestroyClass(vnode) {
-  const classObj = vnode.data.class;
-  const onDestroy = classObj && classObj.destroy;
+  var classObj = vnode.data.class;
+  var onDestroy = classObj && classObj.destroy;
   if (typeof onDestroy !== 'object') {
     return;
   }
 
-  const el = vnode.elm;
-  for (const name in onDestroy) {
+  var el = vnode.elm;
+  for (var name in onDestroy) {
     addOrRemoveClass(el, name, onDestroy[name]);
   }
 }
 
 function applyRemoveClass(vnode, rmFunc) {
-  const classObj = vnode.data.class;
-  const onRemove = classObj && classObj.remove;
+  var classObj = vnode.data.class;
+  var onRemove = classObj && classObj.remove;
   if (typeof onRemove !== 'object') {
     rmFunc();
     return;
   }
 
-  const el = vnode.elm;
-  let delayRemove = 0;
-  for (const name in onRemove) {
-    const val = onRemove[name];
+  var el = vnode.elm;
+  var delayRemove = 0;
+  for (var name in onRemove) {
+    var val = onRemove[name];
     if (name === 'delayRemove') {
       delayRemove = val;
     } else {
